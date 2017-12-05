3e37895581911c0a9e33ff0aa82645332ab955df
VAL <val.shklianik@gmail.com>
2017-12-05 04:54:36 +0300

    calcualtior + tests

---
+++ Calculator/CalculatorLogic.Tests/TestClass.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLogic.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [TestCase("6,4 + 7", ExpectedResult = 13.4)]
+        [TestCase("7   +   11", ExpectedResult = 18)]
+        [TestCase("4 + 002", ExpectedResult = 6)]
+        [TestCase("0+0", ExpectedResult = 0)]
+        [TestCase("7+00010", ExpectedResult = 17)]
+        [TestCase("6,4 *10", ExpectedResult = 64)]
+        [TestCase("0* 15", ExpectedResult = 0)]
+        [TestCase("0*0", ExpectedResult = 0)]
+        [TestCase("6,4 *2", ExpectedResult = 12.8)]
+        [TestCase("6,4 /2", ExpectedResult = 3.2)]
+        [TestCase("6,4 /1", ExpectedResult = 6.4)]
+        [TestCase("0/15", ExpectedResult = 0)]
+        [TestCase("15,3 / 3", ExpectedResult = 5.1)]
+        [TestCase("2 /2", ExpectedResult = 1)]
+        [TestCase("30,4 -20,4", ExpectedResult = 10)]
+        [TestCase("0 -20,4", ExpectedResult = -20.4)]
+        [TestCase("5 -5", ExpectedResult = 0)]
+        [TestCase("20 - 10,0", ExpectedResult = 10)]
+        [TestCase("4 -1", ExpectedResult = 3)]
+        public double AdditionTwoNumbersTest(string exp)
+        {
+            return Calculator.Calculate(exp);
+        }
+
+
+
+        [TestCase("5+2- / *3")]
+        [TestCase("4 + 1 -- 3")]
+        [TestCase("54544515//51515")]
+        public void CalcExceptionTests(string exp)
+        {
+
+            Assert.Throws<FormatException>(() => Calculator.Calculate(exp));
+        }
+    }
+}
---
+++ Calculator/CalculatorLogic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CalculatorLogic
+{
+    public static class Calculator
+    {
+ 
+        public static double Calculate(string expression)
+        {
+            char operation = expression[expression.IndexOfAny(new[] {'+', '-', '/', '*'})];
+            string[] arguments = expression.Split(operation);
+            double a = double.Parse(arguments[0].Trim());
+            double b = double.Parse(arguments[1].Trim());
+
+            switch (operation)
+            {
+                case '+':
+                    return Add(a, b);
+                case '-':
+                    return Add(a, -b);
+                case '*':
+                    return Multiplication(a, b);
+                case '/':
+                    return Multiplication(a, 1/b);
+                default:
+                    throw new ArgumentException("Operation doesn't supported");
+            }
+        }
+
+        private static double Add(double a, double b) => a + b;
+
+        private static double Multiplication(double a, double b) => a * b;
+    }
+}
