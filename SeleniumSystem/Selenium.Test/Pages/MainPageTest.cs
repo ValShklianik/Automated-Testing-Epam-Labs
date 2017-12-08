8ce01e613056a565d8a73593f21d77462d086381
VAL <val.shklianik@gmail.com>
2017-12-08 03:29:40 +0300

    lab4, 5

---
+++ SeleniumSystem/Selenium.Test/Pages/MainPageTest.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using Selenium.Driver;
+using Selenium.Pages;
+
+namespace Selenium.Test.Pages
+{
+    [TestFixture]
+    public class MainPageTest
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Init()
+        {
+            driver = DriverInstance.Get();
+            DriverInstance.SetWaitTime(30);
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            DriverInstance.Close();
+        }
+
+        [Test]
+        public void OpenMainPage()
+        {
+            MainPage mainPage = new MainPage(driver);
+            mainPage.Open();
+
+            Assert.AreEqual(driver.Url, MainPage.URL);
+        }
+
+        
+        [Test]
+        public void LeaveOpinion()
+        {
+            MainPage mainPage = new MainPage(driver);
+            mainPage.Open().LeaveOpinion("Thank you for your work!");
+            IWebElement title = driver.FindElement(By.ClassName("success_title"));
+
+            Assert.IsTrue(title.Displayed);
+        }
+
+        [Test]
+        public void OrderingAirTicketsRules()
+        {
+            MainPage mainPage = new MainPage(driver);
+            mainPage.Open().OrderingAirTicketsRules();
+
+            Assert.AreEqual(driver.Url, AviaRulesPage.URL);
+        }
+
+        [Test]
+        public void OpenAirports()
+        {
+            MainPage mainPage = new MainPage(driver);
+            mainPage.Open().OpenAirports();
+
+            Assert.AreEqual(driver.Url, AirportPage.URL);
+        }
+    }
+}
